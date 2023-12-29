5293817d23b0bfb09cd0d21aa6208f56789e6ecd
Jake Martin <jmartin72358@gmail.com>
2023-12-29 08:59:54 -0600

    front end school work examples

---
+++ Front End School Work/js/resize.js
@@ -0,0 +1,25 @@
+$(function () {
+  setIFrameSize();
+  $(window).resize(function () {
+    setIFrameSize();
+  });
+});
+
+function setIFrameSize() {
+  var ogWidth = 700;
+  var ogHeight = 600;
+  var ogRatio = ogWidth / ogHeight;
+
+  var windowWidth = $(window).width();
+  if (windowWidth < 480) {
+    var parentDivWidth = $(".iframe-class").parent().width();
+    var newHeight = (parentDivWidth / ogRatio);
+    $(".iframe-class").addClass("iframe-class-resize");
+    $(".iframe-class-resize").css("width", parentDivWidth);
+    $(".iframe-class-resize").css("height", newHeight);
+  } else {
+    // $(".iframe-class-resize").removeAttr("width");
+    // $(".iframe-class-resize").removeAttr("height");
+    $(".iframe-class").removeClass("iframe-class-resize").css({ width: '', height: '' })
+  }
+}
